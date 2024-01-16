80fad1c96cb79c1b25d5e604f27fda86790f5da4
Martin Lind <martin@spegelreflex.se>
2024-01-16 16:51:00 +0100

    init-elpa to initalize elpa with melpa repository.
    Extra functions copied from purcell/emacs.d to handle package
    installations. Might not need but looks handy.

--- init.el
+++ init.el
@@ -8,4 +8,16 @@
 (add-to-list 'load-path
 	     (expand-file-name "lisp" user-emacs-directory))
 
+;; Customize emacs behaviour
+(setq inhibit-startup-message t)
+
+
+;; Setup subsystems
+
+(require 'init-elpa) ; Installs required packages.
+
+
+
+(provide 'init)
+
 ;;; init.el ends here
